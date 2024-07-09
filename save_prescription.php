c92f30b4362fa66481eef63e0185ce8ded2372ad
Chokkalingam.s <chok22017.it@rmkec.ac.in>
2024-07-09 13:50:55 +0530

    okie

--- save_prescription.php
+++ save_prescription.php
@@ -4,26 +4,24 @@ $username = "root";
 $password = "";
 $dbname = "bloom";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Fetch POST data
+
 $unique_id = $_POST['unique_id'];
 $medication_prescribed = isset($_POST['medication_prescribed']) ? $_POST['medication_prescribed'] : '';
 $notes = isset($_POST['notes']) ? $_POST['notes'] : '';
 $key_therapies = isset($_POST['key_therapies']) ? $_POST['key_therapies'] : '';
 
-// Check if a prescription already exists for the unique_id
+
 $sql_check = "SELECT * FROM prescription WHERE unique_id = '$unique_id'";
 $result_check = $conn->query($sql_check);
 
 if ($result_check->num_rows > 0) {
-    // Prescription exists, update it
     $sql_update = "UPDATE prescription SET 
                     key_therapies = '$key_therapies', 
                     medication_prescribed = '$medication_prescribed', 
@@ -36,7 +34,6 @@ if ($result_check->num_rows > 0) {
         echo "Error updating prescription: " . $conn->error;
     }
 } else {
-    // Prescription does not exist, insert a new one
     $sql_insert = "INSERT INTO prescription (unique_id, patient_first_name, patient_last_name, key_therapies, medication_prescribed, notes)
                    VALUES ('$unique_id', 
                            (SELECT patient_first_name FROM appointments WHERE unique_id = '$unique_id' LIMIT 1), 
