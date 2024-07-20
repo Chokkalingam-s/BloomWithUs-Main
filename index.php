bff0a0f4a7a8b98c04d3438f948e839e40711f28
Chokkalingam.s <chok22017.it@rmkec.ac.in>
2024-07-20 21:01:43 +0530

    First change

--- index.php
+++ index.php
@@ -22,7 +22,6 @@ $result = $conn->query($sql);
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-  <!-- Fonts -->
   <link href="https://fonts.googleapis.com" rel="preconnect">
   <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
